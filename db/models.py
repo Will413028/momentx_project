2a89334543a00cabe1263dd2813043bf99d5edfe
will <will413028@gmail.com>
2024-03-08 02:10:21 +0800

    feat: add questions_answers table

--- db/models.py
+++ db/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import String
-from sqlalchemy.orm import mapped_column
+from sqlalchemy import String, Text, Integer, ForeignKey
+from sqlalchemy.orm import mapped_column, relationship
 
 from db.database import Base
 
@@ -7,4 +7,14 @@ from db.database import Base
 class Document(Base):
     __tablename__ = 'documents'
 
-    name = mapped_column(String(255), index=True)
+    name = mapped_column(String(255), index=True, nullable=False)
+    questions_answers = relationship("QuestionAnswer", back_populates="document")
+
+
+class QuestionAnswer(Base):
+    __tablename__ = 'questions_answers'
+
+    question = mapped_column(Text, nullable=False)
+    answer = mapped_column(Text, nullable=False)
+    document_id = mapped_column(Integer, ForeignKey('documents.id'))
+    document = relationship("Document", back_populates="questions_answers")
